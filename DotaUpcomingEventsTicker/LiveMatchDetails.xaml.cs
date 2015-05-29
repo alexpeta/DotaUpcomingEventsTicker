ea96a8ab2b085d9b3328fcf5b92123715466e2bf
Alex Peta <alex@alexpeta.ro>
2015-05-29 23:28:53 +0300

    started layers decoupling
    added repositories  to handle data : one for scraping the other for in
    memory static dictionary.

---
+++ DotaUpcomingEventsTicker/LiveMatchDetails.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DotaUpcomingEventsTicker
+{
+    /// <summary>
+    /// Interaction logic for LiveMatchDetails.xaml
+    /// </summary>
+    public partial class LiveMatchDetails : Window
+    {
+        public LiveMatchDetails()
+        {
+            InitializeComponent();
+        }
+    }
+}
